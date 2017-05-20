17017076bb3f181155f3bf1ee93a37c81031e2fe
Mac <mac@Macs-MacBook-Pro.local>
2017-05-21 00:15:48 +0300

    side menu

--- app/src/main/java/com/tasree7a/Fragments/HomeFragment.java
+++ app/src/main/java/com/tasree7a/Fragments/HomeFragment.java
@@ -3,11 +3,14 @@ package com.tasree7a.Fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.tasree7a.Adapters.PopularSallonsAdapter;
@@ -19,11 +22,56 @@ public class HomeFragment extends BaseFragment {
 
     RecyclerView popularSallons;
 
+    ImageView openMenu;
+
+    DrawerLayout nvDrawer;
+
+    NavigationView nvView ;
+
+    ImageView closeDrawer;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        openMenu = (ImageView)rootView.findViewById(R.id.open_menu);
+
+        nvDrawer = (DrawerLayout) rootView.findViewById(R.id.drawer_layout);
+
+        nvView = (NavigationView) rootView.findViewById(R.id.nvView);
+
+        closeDrawer = (ImageView) nvView.getHeaderView(0).findViewById(R.id.close_menu);
+
+        int width = (int)(getResources().getDisplayMetrics().widthPixels/1.5);
+
+        DrawerLayout.LayoutParams params = (android.support.v4.widget.DrawerLayout.LayoutParams) nvView.getLayoutParams();
+
+        params.width = width;
+
+        nvView.setLayoutParams(params);
+
+        openMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nvDrawer.openDrawer(nvView);
+
+            }
+        });
+
+        closeDrawer.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                nvDrawer.closeDrawers();
+
+            }
+        });
+
+
 
         return rootView;
 

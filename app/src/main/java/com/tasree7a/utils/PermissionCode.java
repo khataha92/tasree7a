827369d8cf9ee28491fa60e0d79b1da2cd38c50b
Mac <mac@Macs-MacBook-Pro.local>
2017-06-24 20:35:16 +0300

    fixed apps

---
+++ app/src/main/java/com/tasree7a/utils/PermissionCode.java
@@ -0,0 +1,10 @@
+package com.tasree7a.utils;
+
+/**
+ * Created by mac on 6/14/17.
+ */
+
+public final class PermissionCode {
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 100;
+}

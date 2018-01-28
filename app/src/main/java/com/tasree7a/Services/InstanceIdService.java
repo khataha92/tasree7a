e655cee1d1cf6891c2ba22019311dc564045ebc4
khataha92@github.com <khalid@Khalid-Taha.local>
2018-01-28 02:53:45 +0200

    Added Push notifications to the app

---
+++ app/src/main/java/com/tasree7a/Services/InstanceIdService.java
@@ -0,0 +1,24 @@
+package com.tasree7a.Services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by khalid on 1/28/18.
+ */
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+
+    public static final String TAG = InstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+}
---
+++ app/src/main/java/com/tasree7a/Services/PushService.java
@@ -0,0 +1,12 @@
+package com.tasree7a.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by khalid on 1/28/18.
+ */
+
+public class PushService extends FirebaseMessagingService {
+
+
+}
